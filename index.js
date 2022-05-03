96595378179639e53eac3e256fadd7eb2a012faa
Kevin Nielsen <knielsen24@gmail.com>
2022-05-03 09:07:51 -0700

    commit tree 2c460d8d35269af52eb65f1da5b2b56f08c6159b

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => renderBooks(data))
+  //.catch(error => console.error('Error:', error))
   
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +16,8 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
